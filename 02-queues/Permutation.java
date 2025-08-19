6bf498549e98c788cd3017a2a80725522ce382d3
kedricdaly <49046440+kedricdaly@users.noreply.github.com>
2025-08-19 12:47:05 -0400

    feat(queues): implement Permutation class

--- 02-queues/Permutation.java
+++ 02-queues/Permutation.java
@@ -1,12 +1,32 @@
 /* *****************************************************************************
- *  Name:
- *  Date:
- *  Description:
+ *  Name:              Kedric Daly
+ *  Coursera ID:       N/A
+ *  Last modified:     18 Aug 2025
  **************************************************************************** */
 
-package PACKAGE_NAME;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Permutation {
     public static void main(String[] args) {
-        
+        // k is the number of items to print at most
+        int k = Integer.parseInt(args[0]);
+        if (k == 0) return; // special handling for early exit
+
+        RandomizedQueue<String> rQueue = new RandomizedQueue<String>();
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            rQueue.enqueue(item);
+        }
+
+        int count = 0;
+        for (String permutationItem : rQueue) {
+            StdOut.println(permutationItem);
+            count++;
+            if (count >= k) {
+                return;
+            }
+        }
+
     }
 }
