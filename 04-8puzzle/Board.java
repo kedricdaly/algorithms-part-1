e2f8bd80037c088b0dd521172d594f522c3b9972
kedricdaly <49046440+kedricdaly@users.noreply.github.com>
2026-01-01 16:43:17 -0500

    feat(Board): init constructor and toString() and dimension() methods

--- 04-8puzzle/Board.java
+++ 04-8puzzle/Board.java
@@ -4,19 +4,45 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdOut;
+
 public class Board {
+
+    private int[][] tiles;
+    private int n; // single dimension
+
     // create a board from an n-by-n array of tiles,
     // where tiles[row][col] = tile at (row, col)
-    public Board(int[][] tiles)
+    public Board(int[][] tiles) {
+        this.tiles = tiles;
+        this.n = tiles.length;
+    }
 
     // string representation of this board
-    public String toString()
+    // from https://coursera.cs.princeton.edu/algs4/assignments/8puzzle/faq.php
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(n + "\n");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                s.append(String.format("%2d ", tiles[i][j]));
+            }
+            s.append("\n");
+        }
+        return s.toString();
+    }
 
     // board dimension n
-    public int dimension()
+    public int dimension() {
+        return n;
+    }
 
     // number of tiles out of place
-    public int hamming()
+
+    /*
+    public int hamming() {
+
+    }
 
     // sum of Manhattan distances between tiles and goal
     public int manhattan()
@@ -31,10 +57,16 @@ public class Board {
     public Iterable<Board> neighbors()
 
     // a board that is obtained by exchanging any pair of tiles
-    public Board twin()
+    public Board twin() {
+
+    }
+    */
 
     // unit testing (not graded)
     public static void main(String[] args) {
-
+        int[][] testTiles = new int[][] { { 1, 2 }, { 3, 0 } };
+        Board testBoard = new Board(testTiles);
+        StdOut.println(testBoard.toString());
+        StdOut.println("Dimension: " + testBoard.dimension());
     }
 }
